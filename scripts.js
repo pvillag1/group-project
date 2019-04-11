6a950cb6ab56842307753fbbe92f0d2310264486
latambavarun <42617301+latambavarun@users.noreply.github.com>
2019-04-10 22:44:37 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function onload(){
+    var sectionContact = document.getElementById("contact");
+    sectionContact.style.display='none';
+}
+function loadContact(){
+    var sectionForward =document.getElementById("forward");
+    sectionForward.style.display='none';
+    var sectionContact = document.getElementById("contact");
+    sectionContact.style.display='block';
+}
